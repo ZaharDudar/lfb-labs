5fb72c256ab6d75c821d10af7ed95eaa118de6c1
Zahar <zahar5070@gmail.com>
2024-02-15 15:48:30 +0300

    rename yet_.._cowsays

---
+++ lab3/yet_another_educational_cowsay_local/Parser.hpp
@@ -0,0 +1,34 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <string>
+using std::string;
+
+class Parser {
+
+    string cowsource, cloudsource, message;
+    struct Flags {
+        bool cowsource = false;
+        bool cloudsource = false;
+        bool eyes = false;
+        bool tongue = false;
+        bool snow = false;
+        bool rain = false;
+        bool background = false;
+	      bool ks = false;
+        bool money = false;
+    } flags;
+
+public:
+
+    Parser(int argc, char *argv[]);
+    ~Parser();
+
+    string getCow();
+    string getCloud();
+    char   getFill();
+    char   getSun();
+    string getMessage();
+};
+
+#endif
